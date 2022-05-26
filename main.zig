96b94d30eb97ebf46347adce23f14d25ad53317c
ousttrue <ousttrue@gmail.com>
2022-05-27 02:16:18 +0900

    docking branch

--- main.zig
+++ main.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const glfw = @import("glfw");
+const glfw = @import("pkgs/glfw/src/main.zig");
 const gl = @import("gl");
 const imgui = @import("pkgs/imgui/src/main.zig");
 
@@ -7,10 +7,16 @@ fn getProc(_: ?*glfw.GLFWwindow, name: [:0]const u8) ?*anyopaque {
     return glfw.glfwGetProcAddress(@ptrCast([*]const u8, name));
 }
 
+// fn glfw_error_callback(code: c_int, description: [*]const u8) void {
+//     std.debug.print("Glfw Error {}: {s}", .{ code, description });
+// }
+
 pub fn main() anyerror!void {
     // https://www.glfw.org/documentation.html
 
     // Initialize the library
+    // glfw.glfwSetErrorCallback(@ptrCast(?*?*anyopaque, glfw_error_callback));
+
     if (glfw.glfwInit() == 0)
         @panic("glfwInit");
     defer glfw.glfwTerminate();
@@ -37,9 +43,22 @@ pub fn main() anyerror!void {
     _ = imgui.CreateContext(.{});
     defer imgui.DestroyContext(.{});
     const io = imgui.GetIO() orelse @panic("GetIO");
+    io.ConfigFlags |= @enumToInt(imgui.ImGuiConfigFlags._NavEnableKeyboard); // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= @enumToInt(imgui.ImGuiConfigFlags._DockingEnable); // Enable Docking
+    io.ConfigFlags |= @enumToInt(imgui.ImGuiConfigFlags._ViewportsEnable); // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
 
     // Setup Dear ImGui style
     imgui.StyleColorsDark(.{});
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    var style = imgui.GetStyle() orelse @panic("GetStyle");
+    if ((io.ConfigFlags & @enumToInt(imgui.ImGuiConfigFlags._ViewportsEnable)) != 0) {
+        style.WindowRounding = 0.0;
+        style.Colors[@enumToInt(imgui.ImGuiCol._WindowBg)].w = 1.0;
+    }
 
     // Setup Platform/Renderer backends
     _ = imgui.ImGui_ImplGlfw_InitForOpenGL(@ptrCast(*imgui.GLFWwindow, window), true);
@@ -49,7 +68,7 @@ pub fn main() anyerror!void {
     defer imgui.ImGui_ImplOpenGL3_Shutdown();
 
     // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use imgui.PushFont()/PopFont() to select them.
     // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
     // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
     // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
@@ -67,8 +86,10 @@ pub fn main() anyerror!void {
 
     // Our state
     var show_demo_window = true;
-    // bool show_another_window = false;
+    var show_another_window = false;
     var clear_color: imgui.ImVec4 = .{ .x = 0.45, .y = 0.55, .z = 0.60, .w = 1.00 };
+    var f: f32 = 0.0;
+    var counter: i32 = 0;
 
     // Loop until the user closes the window
     while (glfw.glfwWindowShouldClose(window) == 0) {
@@ -85,6 +106,35 @@ pub fn main() anyerror!void {
             imgui.ShowDemoWindow(.{ .p_open = &show_demo_window });
         }
 
+        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+        {
+            _ = imgui.Begin("Hello, world!", .{}); // Create a window called "Hello, world!" and append into it.
+
+            imgui.Text("This is some useful text."); // Display some text (you can use a format strings too)
+            _ = imgui.Checkbox("Demo Window", &show_demo_window); // Edit bools storing our window open/close state
+            _ = imgui.Checkbox("Another Window", &show_another_window);
+
+            _ = imgui.SliderFloat("float", &f, 0.0, 1.0, .{}); // Edit 1 float using a slider from 0.0f to 1.0f
+            _ = imgui.ColorEdit3("clear color", &clear_color.x, .{}); // Edit 3 floats representing a color
+
+            if (imgui.Button("Button", .{})) // Buttons return true when clicked (most widgets return true when edited/activated)
+                counter += 1;
+            imgui.SameLine(.{});
+            // imgui.Text("counter = %d", counter);
+
+            // imgui.Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0 / imgui.GetIO().Framerate, imgui.GetIO().Framerate);
+            imgui.End();
+        }
+
+        // 3. Show another simple window.
+        if (show_another_window) {
+            _ = imgui.Begin("Another Window", .{ .p_open = &show_another_window }); // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+            imgui.Text("Hello from another window!");
+            if (imgui.Button("Close Me", .{}))
+                show_another_window = false;
+            imgui.End();
+        }
+
         imgui.Render();
         var display_w: c_int = undefined;
         var display_h: c_int = undefined;
@@ -95,6 +145,16 @@ pub fn main() anyerror!void {
         gl.clearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
         gl.clear(gl.COLOR_BUFFER_BIT);
         imgui.ImGui_ImplOpenGL3_RenderDrawData(imgui.GetDrawData());
+
+        // Update and Render additional Platform Windows
+        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+        if ((io.ConfigFlags & @enumToInt(imgui.ImGuiConfigFlags._ViewportsEnable)) != 0) {
+            const backup_current_context = glfw.glfwGetCurrentContext();
+            imgui.UpdatePlatformWindows();
+            imgui.RenderPlatformWindowsDefault(.{});
+            glfw.glfwMakeContextCurrent(backup_current_context);
+        }
 
         // Swap front and back buffers
         glfw.glfwSwapBuffers(window);
