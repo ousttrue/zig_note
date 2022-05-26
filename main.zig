4c30ac5ebf0181a5ac05366029b59132b30bf915
ousttrue <ousttrue@gmail.com>
2022-05-26 19:36:19 +0900

    font load

--- main.zig
+++ main.zig
@@ -36,7 +36,7 @@ pub fn main() anyerror!void {
 
     _ = imgui.CreateContext(.{});
     defer imgui.DestroyContext(.{});
-    _ = imgui.GetIO();
+    const io = imgui.GetIO() orelse @panic("GetIO");
 
     // Setup Dear ImGui style
     imgui.StyleColorsDark(.{});
@@ -47,6 +47,23 @@ pub fn main() anyerror!void {
     defer imgui.ImGui_ImplGlfw_Shutdown();
     _ = imgui.ImGui_ImplOpenGL3_Init(.{ .glsl_version = glsl_version });
     defer imgui.ImGui_ImplOpenGL3_Shutdown();
+
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/ProggyTiny.ttf", 10.0f);
+    if (io.Fonts) |fonts| {
+        const font = fonts.AddFontFromFileTTF("c:\\Windows\\Fonts\\msgothic.ttc", 18.0, null, fonts.GetGlyphRangesJapanese());
+        std.debug.assert(font != null);
+    }
 
     // Our state
     var show_demo_window = true;
